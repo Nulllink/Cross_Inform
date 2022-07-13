43e3359693659faefe8af354296782424a4697e2
Nulllink <alexanveld@gmail.com>
2022-07-13 13:01:21 +0300

    Last full version

--- Cross_Inform/Program.cs
+++ Cross_Inform/Program.cs
@@ -9,7 +9,7 @@ namespace Cross_Inform
 {
     class Program
     {
-        static Dictionary<string,int> triplets = new Dictionary<string, int>();
+        static Dictionary<string,int> triplets0 = new Dictionary<string, int>();
         static Dictionary<string, int> triplets1 = new Dictionary<string, int>();
         static Dictionary<string, int> triplets2 = new Dictionary<string, int>();
         static string[] lines1;
@@ -26,14 +26,34 @@ namespace Cross_Inform
             DateTime start_time = DateTime.Now;
 
             //File_process(path);
-            //all_lines = File.ReadAllLines(path);
+            lines1 = File.ReadAllLines(path);
+            int mid = lines1.Length / 2;
+            Task t1 = Task.Factory.StartNew(() => Line_process(triplets1, 0, mid));
+            Task t2 = Task.Factory.StartNew(() => Line_process(triplets2, mid, lines1.Length));
+            //Task t1 = Task.Factory.StartNew(() => Line_process(lines1[index],triplets1));
+            //index++;
+            //Task t2 = Task.Factory.StartNew(() => Line_process(lines1[index],triplets2));
+            //index++;
+            //while (index < lines1.Length-1)
+            //{
+            //    if (t1.IsCompleted)
+            //    {
+            //        t1 = Task.Factory.StartNew(() => Line_process(lines1[index],triplets1));
+            //        index++;
+            //    }
+            //    if (t2.IsCompleted)
+            //    {
+            //        t2 = Task.Factory.StartNew(() => Line_process(lines1[index],triplets2));
+            //        index++;
+            //    }
+            //}
             //int delt = all_lines.Length / 2;
             //lines1 = new string[delt];
             //lines2 = new string[delt+1];
             //Array.Copy(all_lines, lines1, delt);
             //Array.Copy(all_lines, delt + 1, lines2, 0, delt);
-            Task task = Task.Factory.StartNew(() => Read_file());
-            Task task2 = Task.Factory.StartNew(() => Line_process3(task));
+            //Task task = Task.Factory.StartNew(() => Read_file());
+            //Task task2 = Task.Factory.StartNew(() => Line_process3(task));
             //Task task3 = Task.Factory.StartNew(() => Trip_process());
             //int old_i = 0;
             //while (!task.IsCompleted)
@@ -54,19 +74,21 @@ namespace Cross_Inform
             //{
             //    Thread.Sleep(10);
             //}
-            //foreach (var pair in triplets2)
-            //{
-            //    if (triplets1.ContainsKey(pair.Key))
-            //    {
-            //        triplets1[pair.Key] += pair.Value;
-            //    }
-            //    else
-            //    {
-            //        triplets1.Add(pair.Key, pair.Value);
-            //    }
-            //}
+            t1.Wait();
+            t2.Wait();
+            foreach (var pair in triplets2)
+            {
+                if (triplets1.ContainsKey(pair.Key))
+                {
+                    triplets1[pair.Key] += pair.Value;
+                }
+                else
+                {
+                    triplets1.Add(pair.Key, pair.Value);
+                }
+            }
 
-            task2.Wait();
+            //task2.Wait();
             var sortedDict = (from entry in triplets1 orderby entry.Value descending  select entry).Take(10);
             foreach(var pair in sortedDict)
             {
@@ -87,13 +109,13 @@ namespace Cross_Inform
                     for(int i = 0; i < word.Length - 2; i++)
                     {
                         string trip = word.Substring(i, 3).ToLower();
-                        if (triplets.ContainsKey(trip))
+                        if (triplets0.ContainsKey(trip))
                         {
-                            triplets[trip]++;
+                            triplets0[trip]++;
                         }
                         else
                         {
-                            triplets.Add(trip, 1);
+                            triplets0.Add(trip, 1);
                         }
                         
                     }
@@ -127,15 +149,26 @@ namespace Cross_Inform
                 }
             }
         }
-        static void Line_process(string line)
+        static void Line_process(Dictionary<string,int> triplets,int start, int end)
         {
-            string[] words = line.Split();
-            foreach (string word in words)
-            {
-                for (int i = 0; i < word.Length - 2; i++)
+            for (; start < end; start++) {
+                string line = lines1[start];
+                string[] words = line.Split();
+                foreach (string word in words)
                 {
-                    triplets3.Add(word.Substring(i, 3).ToLower());
-                }
+                    for (int i = 0; i < word.Length - 2; i++)
+                    {
+                        string trip = word.Substring(i, 3).ToLower();
+                        if (triplets.ContainsKey(trip))
+                        {
+                            triplets[trip]++;
+                        }
+                        else
+                        {
+                            triplets.Add(trip, 1);
+                        }
+                    }
+                } 
             }
 
         }
@@ -224,4 +257,4 @@ namespace Cross_Inform
         }
     }
 }
-//1t - 798ms 1t - 725ms 2t - 592ms 2t - 843ms
+//1t - 798ms 1t - 725ms 2t - 592ms 2t - 843ms 2t - 984ms
