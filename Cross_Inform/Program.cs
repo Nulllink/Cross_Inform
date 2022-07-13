b80819043b64874b80f696f1d69a147204c2ec73
Nulllink <alexanveld@gmail.com>
2022-07-14 00:22:59 +0300

    fast version

--- Cross_Inform/Program.cs
+++ Cross_Inform/Program.cs
@@ -10,12 +10,13 @@ namespace Cross_Inform
     class Program
     {
         static string[] lines1;
-        static Dictionary<string, int> triplets3 = new Dictionary<string, int>();
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            int th = 2;
+            int th = 4;
             Dictionary<string, int> triplets1 = new Dictionary<string, int>();
             Dictionary<string, int> triplets2 = new Dictionary<string, int>();
+            Dictionary<string, int> triplets3 = new Dictionary<string, int>();
+            Dictionary<string, int> triplets4 = new Dictionary<string, int>();
             //List<Dictionary<string, int>> triplets = new List<Dictionary<string, int>>();
             Dictionary<string, int>[] triplets = new Dictionary<string, int>[th];
             Console.Write("Inter path to file: ");
@@ -26,36 +27,35 @@ namespace Cross_Inform
             
             int mid = lines1.Length / th;
             Console.WriteLine("t1 " + (DateTime.Now - start_time).ToString());
-            List<Task<Dictionary<string, int>>> tasks = new List<Task<Dictionary<string, int>>>();
-            //triplets.Add(new Dictionary<string, int>());
-            //triplets[0] = new Dictionary<string, int>();
-            for (int i = 0;i < th; i++)
-            {
-                //triplets.Add(new Dictionary<string, int>());
-                triplets[i] = new Dictionary<string, int>();
-                //tasks.Add(Task.Factory.StartNew(() => Line_process(i*mid, i*mid+mid)));//0 7595 7595 15190 15190 22785
-                triplets[i] = await Line_process(i * mid, i * mid + mid);
-                //Task<Dictionary<string, int>> task = Line_process(i * mid, i * mid + mid);
-                //tasks.Add(task);
-            }
+            //List<Task> tasks = new List<Task>();
+            //for (int i = 0; i < th; i++)
+            //{
+            //    //tasks.Add(Task t1 = Task.Factory.StartNew(() => Line_process(triplets1, 0, mid)));
+            //}
+            Task t1 = Task.Factory.StartNew(() => Line_process(triplets1, 0, mid));
+            Task t2 = Task.Factory.StartNew(() => Line_process(triplets2, mid, 2*mid));
+            Task t3 = Task.Factory.StartNew(() => Line_process(triplets3, 2*mid, 3*mid));
+            Task t4 = Task.Factory.StartNew(() => Line_process(triplets4, 3 * mid, lines1.Length));
             Console.WriteLine("t4 " + (DateTime.Now - start_time).ToString());
             //for (int i = 0; i < th; i++)
             //{
             //    tasks[i].Wait();
             //    triplets[i] = tasks[i].Result;
             //}
-            //Task t1 = Task.Factory.StartNew(() => Line_process(triplets1, 0, mid));
-            //Task t2 = Task.Factory.StartNew(() => Line_process(triplets2, mid, lines1.Length));
-            //t1.Wait();
-            //t2.Wait();
+            t1.Wait();
+            t2.Wait();
+            t3.Wait();
+            t4.Wait();
             Console.WriteLine("t2 " + (DateTime.Now - start_time).ToString());
-            for (int i = 1; i < th; i++)
-            {
-                Merging_dictionaries(triplets[0], triplets[i]);
-            }
-            //Merging_dictionaries(triplets1, triplets2);
+            //for (int i = 1; i < th; i++)
+            //{
+            //    Merging_dictionaries(triplets[0], triplets[i]);
+            //}
+            Merging_dictionaries(triplets1, triplets2);
+            Merging_dictionaries(triplets1, triplets3);
+            Merging_dictionaries(triplets1, triplets4);
             Console.WriteLine("t3 " + (DateTime.Now - start_time).ToString());
-            var sortedDict = (from entry in triplets[0] orderby entry.Value descending  select entry).Take(10);
+            var sortedDict = (from entry in triplets1 orderby entry.Value descending  select entry).Take(10);
             foreach(var pair in sortedDict)
             {
                 Console.WriteLine(pair.Key+", "+pair.Value);
@@ -63,32 +63,8 @@ namespace Cross_Inform
             TimeSpan end_time = DateTime.Now - start_time;
             Console.WriteLine($"Run time: {end_time.TotalMilliseconds} ms");
         }
-        //static void Line_process(Dictionary<string,int> triplet,int start, int end)
-        //{
-        //    for (; start < end; start++) {
-        //        string line = lines1[start];
-        //        string[] words = line.Split();
-        //        foreach (string word in words)
-        //        {
-        //            for (int i = 0; i < word.Length - 2; i++)
-        //            {
-        //                string trip = word.Substring(i, 3).ToLower();
-        //                if (triplet.ContainsKey(trip))
-        //                {
-        //                    triplet[trip]++;
-        //                }
-        //                else
-        //                {
-        //                    triplet.Add(trip, 1);
-        //                }
-        //            }
-        //        } 
-        //    }
-
-        //}
-        static async Task<Dictionary<string, int>> Line_process(int start, int end)
+        static void Line_process(Dictionary<string, int> triplet, int start, int end)
         {
-            Dictionary<string, int> triplet = new Dictionary<string, int>();
             for (; start < end; start++)
             {
                 string line = lines1[start];
@@ -109,11 +85,8 @@ namespace Cross_Inform
                     }
                 }
             }
-            await Task.Delay(0);
-            return triplet;
+
         }
-
-
         static void Merging_dictionaries(Dictionary<string,int> in_d, Dictionary<string, int> from_d)
         {
             foreach (var pair in from_d)
@@ -130,4 +103,4 @@ namespace Cross_Inform
         }
     }
 }
-//1t - 798ms 1t - 725ms 2t - 592ms 2t - 843ms 2t - 984ms 2t - 613ms
+//1t - 798ms 1t - 725ms 2t - 592ms 2t - 843ms 2t - 984ms 2t - 613ms 4t = 676ms 3t = 570ms 4t = 472ms
